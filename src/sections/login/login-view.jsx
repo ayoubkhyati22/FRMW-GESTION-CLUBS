88b1355afcaabf065eb28f13dcd49ebb3f3bd811
Ayoub KHYATI <ayoubkhyati89@gmail.com>
2024-09-28 11:30:16 +0100

    TEST LOGO

--- src/sections/login/login-view.jsx
+++ src/sections/login/login-view.jsx
@@ -275,7 +275,7 @@ export default function LoginView() {
         
       }}
     >
-      <img src="public\assets\frmwLOGO.png" alt="" width='240px' style={{marginLeft:20, marginTop:20}} />
+      <img src="/assets/images/avatars/LOGO_KSCT_2020-2.png" alt="" width='240px' style={{marginLeft:20, marginTop:20}} />
 
       <Stack alignItems="center" justifyContent="center" sx={{ height: 1 }}>
         <Card
